6a068942130082cc43bde9af85082fa71dd62a17
Fred-el-Jolo <guillaume.frederic@gmail.com>
2022-09-02 18:56:56 +0200

    Add comments

--- src/core/custom-html-element.ts
+++ src/core/custom-html-element.ts
@@ -1,7 +1,14 @@
 export abstract class CustomHtmlElement extends HTMLElement {
 
+    /**
+     * Dynamic props callbacks map
+     */
     #propsCallbacks: Map<string, Array<Function>>;
 
+    /**
+     * Define attributes that can be updated once the component is initialized
+     * @see https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
+     */
     static get observedAttributes(): string[] {
         throw new Error (`static get observedAttributes() method must be implemented on the class ${this.name}`)
     }
@@ -12,11 +19,25 @@ export abstract class CustomHtmlElement extends HTMLElement {
         this.#propsCallbacks = new Map();
     }
 
+    /**
+     * Helper that initialize the shadow DOM for a Web Component
+     * @see https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM
+     * @param template 
+     * @param shadowOptions 
+     */
     initShadowDom(template: HTMLTemplateElement, shadowOptions: ShadowRootInit = { mode: 'open' }) {
         this.attachShadow(shadowOptions);
         this.shadowRoot?.appendChild(template.content.cloneNode(true));
     }
 
+    /**
+     * Lifecycle hook that is invoked when one of the custom element's attributes is added, removed, or changed
+     * @see https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
+     * @param property 
+     * @param oldValue 
+     * @param newValue 
+     * @returns 
+     */
     attributeChangedCallback(property: string, oldValue: any, newValue: any): void {
         if (oldValue === newValue) return;
         console.debug(`Class ${this.constructor.name}, property ${property} set with value ${newValue}`);
@@ -28,6 +49,11 @@ export abstract class CustomHtmlElement extends HTMLElement {
         }
     }
 
+    /**
+     * Add a callback to be invoked when the key prop is updated
+     * @param property 
+     * @param callback 
+     */
     propertyUpdateCallback(property: string, callback: Function): void {
         let callbacks = this.#propsCallbacks.get(property);
         if (!callbacks) {
